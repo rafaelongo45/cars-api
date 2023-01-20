00d53dce7daa7f73c1ee013b6cdcb67454ec8c1c
Rafael Longo <rafaelongo45@gmail.com>
2023-01-20 10:35:25 -0300

    Creates car model and repository

--- src/main/java/com/cars/api/Controllers/CarsController.java
+++ src/main/java/com/cars/api/Controllers/CarsController.java
@@ -1,22 +1,32 @@
 package com.cars.api.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cars.api.Model.Car;
 import com.cars.api.dto.CarDTO;
+import com.cars.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/car")
 public class CarsController {
 
+  @Autowired
+  private CarRepository carRepository;
+  
   @PostMapping
   public void create(@RequestBody CarDTO req){
-    System.out.println(req.modelo() 
-    + "\n" + req.fabricante() 
-    + "\n" + req.anoModelo() 
-    + "\n" + req.dataFabricacao()
-    + "\n" + req.valor());
+    carRepository.save(new Car(req));
+  }
+
+  @GetMapping
+  public List<Car> listAll(){
+    return carRepository.findAll();
   }
 }
